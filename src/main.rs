0fece7d286e8d7168f37bf9ddf5c5186ced076d4
Daoud Clarke <daoud.clarke@gmail.com>
2023-02-17 22:02:31 +0000

    Implement Karplus Strong

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 extern crate portaudio;
 
 use portaudio as pa;
-use std::f64::consts::PI;
+// use std::f64::consts::PI;
 
 const CHANNELS: i32 = 2;
 const NUM_SECONDS: i32 = 5;
 const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
-const TABLE_SIZE: usize = 200;
+const TABLE_SIZE: usize = 50;
 
 fn main() {
     match run() {
@@ -27,11 +27,13 @@ fn run() -> Result<(), pa::Error> {
 
     // Initialise sinusoidal wavetable.
     let mut sine = [0.0; TABLE_SIZE];
-    for i in 0..TABLE_SIZE {
-        sine[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
+    for i in 0..20 {
+        sine[i] = 1.0;
+        // sine[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
     }
-    let mut left_phase = 0;
-    let mut right_phase = 0;
+    // let mut left_phase = 0;
+    // let mut right_phase = 0;
+
 
     let pa = pa::PortAudio::new()?;
 
@@ -46,16 +48,24 @@ fn run() -> Result<(), pa::Error> {
     let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
         let mut idx = 0;
         for _ in 0..frames {
-            buffer[idx] = sine[left_phase];
-            buffer[idx + 1] = sine[right_phase];
-            left_phase += 1;
-            if left_phase >= TABLE_SIZE {
-                left_phase -= TABLE_SIZE;
+            let new_value = (sine[0] + sine[1]) / 2.01;
+            buffer[idx] = new_value;
+            buffer[idx + 1] = new_value;
+            for i in 0..TABLE_SIZE - 1 {
+                sine[i] = sine[i+1];
             }
-            right_phase += 3;
-            if right_phase >= TABLE_SIZE {
-                right_phase -= TABLE_SIZE;
-            }
+            sine[TABLE_SIZE - 1] = new_value;
+
+            // buffer[idx] = sine[left_phase];
+            // buffer[idx + 1] = sine[right_phase];
+            // left_phase += 1;
+            // if left_phase >= TABLE_SIZE {
+            //     left_phase -= TABLE_SIZE;
+            // }
+            // right_phase += 3;
+            // if right_phase >= TABLE_SIZE {
+            //     right_phase -= TABLE_SIZE;
+            // }
             idx += 2;
         }
         pa::Continue
