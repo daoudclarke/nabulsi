95be0ede8efdb3fe3d7bf6077398b9b38f21d945
Daoud Clarke <daoud.clarke@gmail.com>
2023-02-18 14:22:12 +0000

    Allow setting the frequency

--- src/karplus.rs
+++ src/karplus.rs
@@ -3,9 +3,11 @@ use std::f64::consts::PI;
 
 pub trait Synth {
     fn step(&mut self) -> f64;
+    fn set_freq(&mut self, frequency: f64);
 }
 
-const TABLE_SIZE: usize = 300;
+const SAMPLE_RATE: f64 = 44_100.0;
+const TABLE_SIZE: usize = 4410;
 
 pub struct KarplusStrong {
     sine: [f64; TABLE_SIZE],
@@ -13,6 +15,7 @@ pub struct KarplusStrong {
     count: i32,
     loc: f64,
     loc_int: i32,
+    freq: f64,
 }
 
 impl KarplusStrong {
@@ -23,6 +26,7 @@ impl KarplusStrong {
             count: 0,
             loc: 0.0,
             loc_int: 0,
+            freq: 660.0,
         };
 
         for i in 0..20 {
@@ -36,13 +40,16 @@ impl KarplusStrong {
 impl Synth for KarplusStrong {
     fn step(self: &mut KarplusStrong) -> f64 {
         self.count += 1;
-        let speed = 2.0 + (self.count as f64 / 10000.0 * PI * 2.0).sin() * (10000.0 / (self.count as f64 + 100000.0));
+        let freq = self.freq + (self.count as f64 / 20000.0 * PI * 2.0).sin() * 2.0;
+        // let speed = 2.0 + (self.count as f64 / 10000.0 * PI * 2.0).sin() * (10000.0 / (self.count as f64 + 100000.0));
+        // let vibrato = (self.count as f64 / 10000.0 * PI * 2.0).sin() * (10000.0 / (self.count as f64 + 100000.0));
+        let speed = freq * (TABLE_SIZE as f64 / SAMPLE_RATE);
         self.loc += speed;
 
         let new_iterations = self.loc as i32 - self.loc_int;
         self.loc_int = self.loc as i32;
 
-        let new_value = (self.sine[self.phase] + self.sine[(self.phase + 1) % TABLE_SIZE]) / 2.001;
+        let new_value = (self.sine[self.phase] + self.sine[(self.phase + 1) % TABLE_SIZE]+ self.sine[(self.phase + 2) % TABLE_SIZE]) / 3.001;
         for _ in 0..new_iterations {
             self.sine[(self.phase + TABLE_SIZE - 1) % TABLE_SIZE] = new_value;
 
@@ -53,5 +60,9 @@ impl Synth for KarplusStrong {
         }
 
         new_value
+    }
+
+    fn set_freq(&mut self, frequency: f64) {
+        self.freq = frequency;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ const CHANNELS: i32 = 2;
 const NUM_SECONDS: i32 = 5;
 const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
-const TABLE_SIZE: usize = 300;
 
 fn main() {
     match run() {
@@ -37,13 +36,17 @@ fn run() -> Result<(), pa::Error> {
     settings.flags = pa::stream_flags::CLIP_OFF;
 
     let mut synth = KarplusStrong::new();
+    let mut synth2 = KarplusStrong::new();
+
+    synth.set_freq(220.0);
+    synth2.set_freq(220.0 + (220.0 / 4.0));
 
     // This routine will be called by the PortAudio engine when audio is needed. It may called at
     // interrupt level on some machines so don't do anything that could mess up the system like
     // dynamic resource allocation or IO.
     let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
         for i in 0..frames {
-            let new_value = synth.step();
+            let new_value = (synth.step() + synth2.step()) / 2.0;
 
             buffer[i*2] = new_value as f32;
             buffer[i*2 + 1] = new_value as f32;
